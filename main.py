d144dae51f365543f40d07c73b38982d720282bf
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-06 14:00:29 +0700

    Python Rename File

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 if __name__ == "__main__":
-  print("Python Rename File")
+  # Python Rename File
+
+  import os
+
+  try:
+    os.rename("readme.txt", "notes.txt")
+  except FileNotFoundError as error:
+    print(error)
+  except FileExistsError as error:
+    print(error)
